fc7ee330962b8ac9541e81066adfd20f299f230b
zenithcoder0 <zenithcoder0@gmail.com>
2025-05-27 13:34:06 -0500

    New card component that uses mapping for data items

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import './App.css'
+import Card from './components/Card'
 import Navbar from './components/Navbar'
+
+
+const cardData = [
+  {title: "Initiative Tracker", summary: "Base html, css, js initiative tracker", link: "",},
+  {title: "Initiative Tracker 2", summary: "Revisited iniative tracker,", link: "",},
+  {title: "Inventory Tracker", summary: "", link: "",}
+];
 
 function App() {
   return (
     <>
       <Navbar/>
+      <section style={{display: "flex", flexDirection: "row", gap: '1rem', justifyContent: 'center'}}>
+        {cardData.map((item, index) => (
+          <Card 
+          key={index}
+          title={item.title}
+          summary={item.summary}
+          link={item.link}
+          />
+        ))}
+      </section>
       <div>Hello World</div>
     </>
   )
---
+++ src/components/Card.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+type BasicCardProps = {
+    title: string;
+    summary: string;
+    link: string;
+}
+
+export default function BasicCard({title, summary, link}: BasicCardProps) {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+          {title}
+        </Typography>
+        <Typography variant="body2">
+          {summary}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">{link}</Button>
+      </CardActions>
+    </Card>
+  );
+}
