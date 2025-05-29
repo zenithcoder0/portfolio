edf5ba8d10645d416143aaaeaceb488a24c21db4
zenithcoder0 <zenithcoder0@gmail.com>
2025-05-29 09:00:42 -0500

    WIP for card visuals

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import Navbar from './components/Navbar'
 
 
 const cardData = [
-  {title: "Initiative Tracker", summary: "Base html, css, js initiative tracker", link: "",},
-  {title: "Initiative Tracker 2", summary: "Revisited iniative tracker,", link: "",},
-  {title: "Inventory Tracker", summary: "", link: "",}
+  {title: "Initiative Tracker", summary: "The Initiative Tracker is a tool to simplify and accelerate combat encounters in tabletop role-playing games like Dungeons & Dragons.\nThis web-based application allows Game Masters to enter and manage multiple combatants quickly, ensuring fair and accurate turn order resolution.", link: "https://zenithcoder0.github.io/initiative-tracker/",},
+  {title: "Initiative Tracker 2", summary: "Revisited iniative tracker,", link: "https://zenithcoder0.github.io/initiative-tracker-2/",},
+  {title: "Inventory Tracker", summary: "In Progress", link: "",}
 ];
 
 function App() {
   return (
     <>
-      <Navbar/>
+      {/* <Navbar/> */}
+      <div>Heres my stuff. This is a work in progress</div>
       <section style={{display: "flex", flexDirection: "row", gap: '1rem', justifyContent: 'center'}}>
         {cardData.map((item, index) => (
           <Card 
@@ -23,7 +24,6 @@ function App() {
           />
         ))}
       </section>
-      <div>Hello World</div>
     </>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,27 +3,39 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
+import { CardMedia } from '@mui/material';
 
 type BasicCardProps = {
     title: string;
     summary: string;
     link: string;
+    image: string;
 }
 
 export default function BasicCard({title, summary, link}: BasicCardProps) {
   return (
     <Card sx={{ minWidth: 275 }}>
+       <CardMedia
+        component="img"
+        height="140"
+        image={image}
+        alt={title}
+      />
       <CardContent>
         <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
           {title}
         </Typography>
         <Typography variant="body2">
-          {summary}
+          {summary.split('\n').map((line, index) => (
+            <span key={index}>
+              {line}
+              <br/>
+            </span>
+          ))}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">{link}</Button>
+      <Button size="small" href={link} target="_blank" rel="noopener noreferrer">Check it out!</Button>
       </CardActions>
     </Card>
   );
