88717b7380cb8e5fd37e467d101937d05b0807b6
zenithcoder0 <zenithcoder0@gmail.com>
2025-05-29 10:18:29 -0500

    Clean version 1!

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import './App.css'
 import TrackerImage from './assets/Initiative-Tracker.png'
+import TrackerImage2 from './assets/Intiiative-Tracker2.png'
+import Sisyphus from './assets/Favicon.png'
+import WIP from './assets/WIP.png'
 import BasicCard from './components/Card';
+import Navbar from './components/Navbar';
 
 const cardData = [
   {title: "Initiative Tracker", 
-    summary: "The Initiative Tracker is a tool to simplify and accelerate combat encounters in tabletop role-playing games like Dungeons & Dragons.\nThis web-based application allows Game Masters to enter and manage multiple combatants quickly, ensuring fair and accurate turn order resolution.", 
+    summary: "The Initiative Tracker is a tool to simplify and accelerate combat encounters in tabletop role-playing games like Dungeons & Dragons.\n\nThis web-based application allows Game Masters to enter and manage multiple combatants quickly, ensuring fair and accurate turn order resolution.", 
     link: "https://zenithcoder0.github.io/initiative-tracker/", 
     image: TrackerImage},
-  {title: "Initiative Tracker 2", summary: "Revisited iniative tracker,", link: "https://zenithcoder0.github.io/initiative-tracker-2/",},
-  {title: "Inventory Tracker", summary: "In Progress", link: "",},
-  {title: "Inventory Tracker", summary: "In Progress", link: "",},
-  {title: "Inventory Tracker", summary: "In Progress", link: "",},
-  {title: "Inventory Tracker", summary: "In Progress", link: "",},
-  {title: "Inventory Tracker", summary: "In Progress", link: "",},
-  {title: "Inventory Tracker", summary: "In Progress", link: "",}
+  {title: "Initiative Tracker 2", summary: "Version 2:\nA simple web app for managing turn order in tabletop RPGs.\n\nAdd characters, track initiative, update health, and progress rounds with ease.\n\nBuilt as a learning tool for game masters who want to spend less time tracking and more time playing.", link: "https://zenithcoder0.github.io/initiative-tracker-2/", image: TrackerImage2},
+  {title: "CRUD Inventory", summary: "In Progress\nThis is going to be my first full-stack developed application.\n\n Potential Techstack:\nFrontend:Type\nBackend: C#\nDB: PostgreSQL\n\n\n I'll figure something out", link: "", image: WIP},
+  {title: "My Portfolio!", summary: "You're already here! :D", link: "", image: Sisyphus},
+ 
 ];
 
 function App() {
   return (
     <>
+    <Navbar/>
       <div>Heres my stuff. This is a work in progress</div>
       <section className="cards-container">
         {cardData.map((item, index) => (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import type { Theme } from '@mui/material/styles';
 
 export default function ButtonAppBar() {
@@ -12,19 +9,9 @@ export default function ButtonAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed" sx={(theme: Theme) => ({zIndex: theme.zIndex.drawer + 1,})}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            PORTFOLIO - ZENITHCODER0
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
